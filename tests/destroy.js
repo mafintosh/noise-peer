66742871645f09ebbfdbae04452d02c2f598aa37
Mathias Buus <mathiasbuus@gmail.com>
2018-10-16 22:01:05 +0200

    fix destroy tests

--- tests/destroy.js
+++ tests/destroy.js
@@ -3,16 +3,23 @@ var peer = require('..')
 var transport = require('./helpers/chopped-stream')
 
 test('simple example', function (assert) {
+  assert.plan(3)
+
   var t = transport()
 
   const a = peer(t.a, true)
   const b = peer(t.b, false)
+
+  a.on('error', function () {
+    assert.pass('a errored')
+  })
+  b.on('close', function () {
+    assert.pass('b closed')
+  })
 
   a.write('hi')
   b.on('data', function (data) {
     assert.same(data, Buffer.from('hi'))
     b.destroy()
   })
-
-  b.on('close', assert.end)
 })
